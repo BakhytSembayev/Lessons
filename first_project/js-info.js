49f238abca60e53a0b29db5dda5ff0a889ae2aea
BakhytSembayev <119586792+BakhytSembayev@users.noreply.github.com>
2023-09-04 21:52:22 +0600

    Add files via upload

---
+++ first_project/js-info.js
@@ -0,0 +1,5 @@
+alert ('JavaScript был создан автором Бренданом Айком (Brandan Eich)');
+alert ('за 10 дней');
+alert ('в 1995 году');
+alert ('JavaScript - это не Java');
+alert ('Спецификация (формальное описание синтаксиса, базовых объектов и алгоритмов) языка JavaScript называется ECMAScript.')
